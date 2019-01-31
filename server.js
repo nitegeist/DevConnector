13b0a8f6bf647b98d3efeb21b894a7cdd5492d30
Cyaxeres <delroybicknell@gmail.com>
2019-01-30 20:40:08 -0500

    Added Deployment ready scripts

--- server.js
+++ server.js
@@ -1,42 +1,49 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const passport = require('passport')
-require('dotenv').config()
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const passport = require('passport');
+require('dotenv').config();
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const path = require('path');
+const app = express();
 
-const app = express()
+const db = require('./config/keys').mongoURI;
 
-const db = require('./config/keys').mongoURI
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log('Database Connected'))
-  .catch(err => console.log(err))
-mongoose.set('debug', true)
+  .catch(err => console.log(err));
+mongoose.set('debug', true);
 
-app.use(passport.initialize())
+app.use(passport.initialize());
 
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
-app.use('/api/users', users)
-app.use('/api/profile', profile)
-app.use('/api/posts', posts)
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-const port = process.env.PORT || 5000
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`)
-})
+  console.log(`Server running on http://localhost:${port}/`);
+});
 
 // Run app, then load http://localhost:port in a browser to see the output.
