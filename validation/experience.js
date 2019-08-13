475e99949414a20a2ef6bc541ec35f141aaa057f
Cyaxeres <delroybicknell@gmail.com>
2019-08-13 12:04:46 -0500

    minor changes

--- validation/experience.js
+++ validation/experience.js
@@ -1,27 +1,27 @@
-const Validator = require('validator')
-const isEmpty = require('./is-empty')
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
 
 const validateExperienceInput = data => {
-  let errors = {}
+  const errors = {};
 
-  data.title = !isEmpty(data.title) ? data.title : ''
-  data.company = !isEmpty(data.company) ? data.company : ''
-  data.from = !isEmpty(data.from) ? data.from : ''
+  data.title = !isEmpty(data.title) ? data.title : '';
+  data.company = !isEmpty(data.company) ? data.company : '';
+  data.from = !isEmpty(data.from) ? data.from : '';
 
   if (Validator.isEmpty(data.title)) {
-    errors.title = 'Job title field is required'
+    errors.title = 'Job title field is required';
   }
   if (Validator.isEmpty(data.company)) {
-    errors.company = 'Company field is required'
+    errors.company = 'Company field is required';
   }
   if (Validator.isEmpty(data.from)) {
-    errors.from = 'From date field is required'
+    errors.from = 'From date field is required';
   }
 
   return {
     errors,
-    isValid: isEmpty(errors)
-  }
-}
+    isValid: isEmpty(errors),
+  };
+};
 
-module.exports = validateExperienceInput
+module.exports = validateExperienceInput;
